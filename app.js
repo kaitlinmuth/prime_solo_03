e05a7bd17c1340d08529360d66583ac1c3f12313
kaitlinmuth <kaitlin.muth@gmail.com>
2015-04-22 00:42:07 -0500

    Prettier

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //Demo of the .css method of JQuery
 $(document).ready(function(){
 	$("#sBtn").on('click', function(event){
-		console.log("submit clicked")
 		var hexVal = "#" + document.getElementById("hexValue").value;
 		event.preventDefault();
 		$("#btn").css("background-color", hexVal);
